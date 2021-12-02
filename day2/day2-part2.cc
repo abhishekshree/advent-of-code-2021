c7c7db80a374183b2b9f6268929e742b7a9bd5f8
Abhishek Shree <abhi1234456@gmail.com>
2021-12-02 21:36:07 +0530

    Day 2 solutions done

---
+++ day2/day2-part2.cc
@@ -0,0 +1,96 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC optimization ("unroll-loops")
+#pragma GCC optimize ("-ffloat-store")
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Data types.
+using ll = long long;
+using vi = vector<int>;
+using vll = vector<ll>;
+using vvl = vector<vll>;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vpll = vector<pll>;
+using mii = map<int, int>;
+using mll = map<ll, ll>;
+
+// No REPs please.
+#define endl            "\n"
+#define sz(x)           (int)((x).size())
+#define all(v)          v.begin(), v.end()
+#define rev_all(v)      v.rbegin(), v.rend()
+#define srt(v)          sort(all(v))
+#define rev_srt(v)      sort(rev_all(v))
+#define unq(v)          (v).erase(unique(all(v)), v.end())
+
+// Not so amazing, but it helps.
+#define mp              make_pair
+#define pb              push_back
+#define pc(x)           __builtin_popcount(x)
+#define pcl(x)          __builtin_popcountll(x)
+
+// Magic.
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+
+// Debugging.
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+
+#ifndef ONLINE_JUDGE 
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+// Constants.
+const int MOD = 1e9 + 7;
+const int INF = 1e9;
+const ll LINF = 1e18;
+const double pi=acos(-1.0);
+
+
+// #define MULTITESTS
+void solve(){
+    freopen("input2.txt", "r", stdin);
+    int hz = 0, vd = 0;
+    int aim = 0;
+    while (true) {
+        string s;
+        cin >> s;
+        int x;
+        cin >> x;
+        if(s == "forward"){
+            hz += x;
+            vd += aim * x;
+        }else if (s == "up"){
+            if (vd != 0) aim -= x;
+        }else {
+            aim += x;
+        }
+        if(  cin.eof() ) break;
+    }
+    cout << hz * vd << endl;
+}
+
+int main(int argc, char const *argv[])
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    #ifdef MULTITESTS
+    int tc; cin >> tc;
+    for(int i = 1; i <= tc; i++) {
+        // cout << "Case #" << i << ": ";
+        solve();
+    }
+    #else
+    solve();
+    #endif
+
+    return 0;
+}
