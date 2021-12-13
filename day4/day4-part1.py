c6797729bb3166aaee1835a1a49c104d7fec22e7
Abhishek Shree <abhi1234456@gmail.com>
2021-12-13 22:16:19 +0530

    Fix Indent

--- day4/day4-part1.py
+++ day4/day4-part1.py
@@ -8,8 +8,8 @@ with open(infile, "r") as f:
     cnt = 0
     board = []
     for l in range(2, len(lines)):
-        if lines[l] != '\n':
-            board.append(list(map(int, lines[l].rstrip('\n').split(" "))))
+        if lines[l] != "\n":
+            board.append(list(map(int, lines[l].rstrip("\n").split(" "))))
         else:
             boards.append(board)
             board = []
@@ -17,17 +17,31 @@ with open(infile, "r") as f:
 
 checkboxes = [[[False for x in range(5)] for y in range(5)] for x in range(len(boards))]
 
+
 def check_win(checkbox):
     ok = False
     for x in range(5):
-        if checkbox[x][0] == True and checkbox[x][1] == True and checkbox[x][2] == True and checkbox[x][3] == True and checkbox[x][4] == True:
+        if (
+            checkbox[x][0] == True
+            and checkbox[x][1] == True
+            and checkbox[x][2] == True
+            and checkbox[x][3] == True
+            and checkbox[x][4] == True
+        ):
             ok = True
-    
+
     for y in range(5):
-        if checkbox[0][y] == True and checkbox[1][y] == True and checkbox[2][y] == True and checkbox[3][y] == True and checkbox[4][y] == True:
+        if (
+            checkbox[0][y] == True
+            and checkbox[1][y] == True
+            and checkbox[2][y] == True
+            and checkbox[3][y] == True
+            and checkbox[4][y] == True
+        ):
             ok = True
-    
+
     return ok
+
 
 def mark(n):
     for z in range(len(boards)):
@@ -35,11 +49,13 @@ def mark(n):
             for y in range(5):
                 if boards[z][x][y] == n:
                     checkboxes[z][x][y] = True
-                
+
+
 def check_winner():
     for c in checkboxes:
         if check_win(c):
             return checkboxes.index(c)
+
 
 win_num = -1
 win_board_idx = -1
@@ -64,4 +80,4 @@ for x in range(5):
         if checkboxes[win_board_idx][x][y] == False:
             s += win_board[x][y]
 
-print("Result: ", s*win_num)
+print("Result: ", s * win_num)
